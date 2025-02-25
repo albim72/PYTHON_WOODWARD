4eb35fea4b0a5f4dfa6f83bf6b531b7fa0942c0e
albim72 <marcin.albiniak72@gmail.com>
2025-02-25 11:26:17 +0100

    Create main.py

---
+++ DZIEN_2/FIGURY_GEOMETRYCZNE/main.py
@@ -0,0 +1,22 @@
+from trojkat import Trojkat
+from prostokat import Prostokat
+from trapez import Trapez
+
+"""
+Komentarz dokumentacyjny
+xyz
+"""
+
+tr = Trojkat(5.6,7.8)
+print(f"pole figury {tr.__class__.__name__} wynosi: {tr.policz_pole():.2f} cm2")
+
+pr1 = Prostokat(7.3,3.9)
+print(f"pole figury {pr1.__class__.__name__} wynosi: {pr1.policz_pole():.2f} cm2")
+print(type(pr1))
+
+pr2 = Prostokat(5,5)
+print(f"pole figury {pr2.__class__.__name__} wynosi: {pr2.policz_pole():.2f} cm2")
+print(type(pr2))
+
+trp = Trapez(7.6,5.7,4.4)
+print(f"pole figury {trp.__class__.__name__} wynosi: {trp.policz_pole():.2f} cm2")
