bdad7bfc02e68ecb220ea5c4443138b18851ad22
albim72 <marcin.albiniak72@gmail.com>
2026-02-04 11:13:35 +0100

    Update main.py

--- DZIEN_2/FIGURY_GEOMETRYCZNE/main.py
+++ DZIEN_2/FIGURY_GEOMETRYCZNE/main.py
@@ -3,24 +3,19 @@ from prostokat import Prostokat
 from trapez import Trapez
 from kolo import Kolo
 
-"""
-Komentarz dokumentacyjny
-xyz
-"""
+tr = Trojkat(5,7)
+print(f"pole figry: {tr.__class__.__name__} wynosi: {tr.oblicz_pole():.2f} cm2")
 
-tr = Trojkat(5.6,7.8)
-print(f"pole figury {tr.__class__.__name__} wynosi: {tr.policz_pole():.2f} cm2")
+pr1 = Prostokat(3.5,4.7)
+print(f"pole figry: {pr1.__class__.__name__} wynosi: {pr1.oblicz_pole():.2f} cm2")
+print(f"typ obiektu pr1: {type(pr1)}")
 
-pr1 = Prostokat(7.3,3.9)
-print(f"pole figury {pr1.__class__.__name__} wynosi: {pr1.policz_pole():.2f} cm2")
-print(type(pr1))
+pr2 = Prostokat(5.1,5.1)
+print(f"pole figry: {pr2.__class__.__name__} wynosi: {pr2.oblicz_pole():.2f} cm2")
+print(f"typ obiektu pr2: {type(pr2)}")
 
-pr2 = Prostokat(5,5)
-print(f"pole figury {pr2.__class__.__name__} wynosi: {pr2.policz_pole():.2f} cm2")
-print(type(pr2))
+trp = Trapez(8.5,5.3,4.4)
+print(f"pole figry: {trp.__class__.__name__} wynosi: {trp.oblicz_pole():.2f} cm2")
 
-trp = Trapez(7.6,5.7,4.4)
-print(f"pole figury {trp.__class__.__name__} wynosi: {trp.policz_pole():.2f} cm2")
-
-kl = Kolo(6.6)
-print(f"pole figury {kl.__class__.__name__} wynosi: {kl.policz_pole():.2f} cm2")
+kl = Kolo(5.5)
+print(f"pole figry: {kl.__class__.__name__} wynosi: {kl.oblicz_pole():.2f} cm2")
